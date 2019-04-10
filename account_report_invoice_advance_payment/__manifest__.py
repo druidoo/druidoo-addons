606aa82b02211bfedabab0711a32e5350614d5a7
Iván Todorovich <ivan.todorovich@gmail.com>
2019-04-10 16:17:12 +0200

    ADD account_report_invoice_advance_payment (#6)
    ADD account_report_invoice_advance_payment

---
+++ account_report_invoice_advance_payment/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Account Report Invoice Advance Payment',
+    'summary': 'This module moves the advance payment lines to the bottom of the invoice.',
+    'version': '12.0.1.0.0',
+    'category': 'Druidoo',
+    'author': 'Iván Todorovich',
+    'website': 'http://www.druidoo.io',
+    'license': 'AGPL-3',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'reports/account_invoice.xml',
+    ],
+}
