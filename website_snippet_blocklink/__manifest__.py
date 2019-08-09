390175e377e7c9a8fed2107718a349b828c114e0
Iván Todorovich <ivan.todorovich@gmail.com>
2019-08-09 11:26:18 +0000

    ADD website_snippet_blocklink

---
+++ website_snippet_blocklink/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Website Snippet Blocklink',
+    'description': 'Provides a blocklink snippet',
+    'category': 'Website',
+    'version': '12.0.1.0.0',
+    'author': 'Druidoo',
+    'license': 'OPL-1',
+    'depends': [
+        'website',
+    ],
+    'data': [
+        'views/assets.xml',
+        'views/snippets.xml',
+        'views/snippet.options.xml',
+    ],
+}
