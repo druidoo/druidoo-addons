4c25d74cbcdffe9eb06b44d0a108212a5f31f4b5
Iván Todorovich <ivan.todorovich@gmail.com>
2019-08-12 14:54:02 +0000

    Bump version

--- pos_order_mgmt_draft_downpayment/__manifest__.py
+++ pos_order_mgmt_draft_downpayment/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     'name': 'POS Order Draft Downpayment',
-    'version': '12.0.1.0.0',
+    'version': '12.0.2.0.0',
     'category': 'Point of Sale',
     'website': 'https://github.com/druidoo/druidoo-addons',
     'author': 'Druidoo',
