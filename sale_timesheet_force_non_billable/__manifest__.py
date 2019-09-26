f56794f88f811f5ff5b137d387380fb54225d4a5
Krunal Prajapati <krunal267@gmail.com>
2019-09-26 22:25:28 +0530

    [ADD]Added a new module to set a Timesheet Billable Type to Non Billable forcefully (#14)
    * [ADD]Added a new module to set a Timesheet Billable Type to Non Billable forcefully

---
+++ sale_timesheet_force_non_billable/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2019-Today: La Louve (<https://cooplalouve.fr>)
+# Copyright (C) 2019-Today: Druidoo (<https://www.druidoo.io>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Project Tasks Extension",
+    "version": "12.0.1.0.0",
+    "author": "Druidoo",
+    "category": "Project",
+    "website": "https://druidoo.io",
+    "license": "AGPL-3",
+    "depends": [
+        'sale_timesheet',
+    ],
+    "data": [
+        'views/account_analytic_line_views.xml',
+    ],
+    "installable": True,
+}
