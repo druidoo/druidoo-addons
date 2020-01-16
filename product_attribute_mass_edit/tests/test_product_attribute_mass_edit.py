835f640fba9c5697f82440d8f0c9178cff6e0ae1
Iván Todorovich <ivan.todorovich@gmail.com>
2020-01-16 15:19:07 -0300

    [REF] product_attribute_mass_edit: Black python code

--- product_attribute_mass_edit/tests/test_product_attribute_mass_edit.py
+++ product_attribute_mass_edit/tests/test_product_attribute_mass_edit.py
@@ -6,128 +6,179 @@ from odoo.exceptions import UserError
 
 
 class TestProductAttributeMassEdit(TransactionCase):
-
     def setUp(self):
         super(TestProductAttributeMassEdit, self).setUp()
 
         self.product1 = self.env.ref(
-            'product.product_product_1_product_template')
+            "product.product_product_1_product_template"
+        )
         self.product2 = self.env.ref(
-            'product.product_product_2_product_template')
+            "product.product_product_2_product_template"
+        )
         self.product3 = self.env.ref(
-            'product.product_product_3_product_template')
+            "product.product_product_3_product_template"
+        )
         self.product4 = self.env.ref(
-            'product.product_product_4_product_template')
+            "product.product_product_4_product_template"
+        )
 
-        self.product_tmpl_ids = \
+        self.product_tmpl_ids = (
             self.product1 + self.product2 + self.product3 + self.product4
+        )
 
-        self.attribute1 = self.env['product.attribute'].create({
-            'name': 'Variant Attribute',
-            'create_variant': 'always',
-            'value_ids': [
-                (0, 0, {'name': 'Variant Value 1'}),
-                (0, 0, {'name': 'Variant Value 2'}),
-            ]})
+        self.attribute1 = self.env["product.attribute"].create(
+            {
+                "name": "Variant Attribute",
+                "create_variant": "always",
+                "value_ids": [
+                    (0, 0, {"name": "Variant Value 1"}),
+                    (0, 0, {"name": "Variant Value 2"}),
+                ],
+            }
+        )
 
-        self.attribute2 = self.env['product.attribute'].create({
-            'name': 'Informational Attribute',
-            'create_variant': 'no_variant',
-        })
+        self.attribute2 = self.env["product.attribute"].create(
+            {
+                "name": "Informational Attribute",
+                "create_variant": "no_variant",
+            }
+        )
 
-        self.attribute2_1 = self.env['product.attribute.value'].create({
-            'attribute_id': self.attribute2.id,
-            'name': 'Info Value 1',
-        })
+        self.attribute2_1 = self.env["product.attribute.value"].create(
+            {"attribute_id": self.attribute2.id, "name": "Info Value 1",}
+        )
 
-        self.attribute2_2 = self.env['product.attribute.value'].create({
-            'attribute_id': self.attribute2.id,
-            'name': 'Info Value 2',
-        })
+        self.attribute2_2 = self.env["product.attribute.value"].create(
+            {"attribute_id": self.attribute2.id, "name": "Info Value 2",}
+        )
 
     def get_wizard(self):
-        return self.env['product.attribute.mass.edit.wizard'].with_context(
-            active_ids=self.product_tmpl_ids.ids).create({})
+        return (
+            self.env["product.attribute.mass.edit.wizard"]
+            .with_context(active_ids=self.product_tmpl_ids.ids)
+            .create({})
+        )
 
     def test_without_variant_creation(self):
         # Test adding new values
         wizard = self.get_wizard()
-        wizard.line_ids = [(0, 0, {
-            'action': 'add',
-            'attribute_id': self.attribute2.id,
-            'value_ids': self.attribute2_1,
-            })]
+        wizard.line_ids = [
+            (
+                0,
+                0,
+                {
+                    "action": "add",
+                    "attribute_id": self.attribute2.id,
+                    "value_ids": self.attribute2_1,
+                },
+            )
+        ]
         wizard.confirm()
         for product in self.product_tmpl_ids:
             attribute_line_id = self.product1.attribute_line_ids.filtered(
-                lambda l: l.attribute_id == self.attribute2)
-            self.assertEqual(
-                attribute_line_id.value_ids, self.attribute2_1)
+                lambda l: l.attribute_id == self.attribute2
+            )
+            self.assertEqual(attribute_line_id.value_ids, self.attribute2_1)
 
         # Test replacing values
         wizard = self.get_wizard()
-        wizard.line_ids = [(0, 0, {
-            'action': 'replace',
-            'attribute_id': self.attribute2.id,
-            'value_ids': self.attribute2.value_ids,
-            })]
+        wizard.line_ids = [
+            (
+                0,
+                0,
+                {
+                    "action": "replace",
+                    "attribute_id": self.attribute2.id,
+                    "value_ids": self.attribute2.value_ids,
+                },
+            )
+        ]
         wizard.confirm()
         for product in self.product_tmpl_ids:
             attribute_line_id = self.product1.attribute_line_ids.filtered(
-                lambda l: l.attribute_id == self.attribute2)
+                lambda l: l.attribute_id == self.attribute2
+            )
             self.assertEqual(
-                attribute_line_id.value_ids, self.attribute2.value_ids)
+                attribute_line_id.value_ids, self.attribute2.value_ids
+            )
 
         # Test removing values
         wizard = self.get_wizard()
-        wizard.line_ids = [(0, 0, {
-            'action': 'remove',
-            'attribute_id': self.attribute2.id,
-            'value_ids': self.attribute2_1,
-            })]
+        wizard.line_ids = [
+            (
+                0,
+                0,
+                {
+                    "action": "remove",
+                    "attribute_id": self.attribute2.id,
+                    "value_ids": self.attribute2_1,
+                },
+            )
+        ]
         wizard.confirm()
         for product in self.product_tmpl_ids:
             attribute_line_id = self.product1.attribute_line_ids.filtered(
-                lambda l: l.attribute_id == self.attribute2)
-            self.assertEqual(
-                attribute_line_id.value_ids, self.attribute2_2)
+                lambda l: l.attribute_id == self.attribute2
+            )
+            self.assertEqual(attribute_line_id.value_ids, self.attribute2_2)
 
         # Test removing attribute
         wizard = self.get_wizard()
-        wizard.line_ids = [(0, 0, {
-            'action': 'replace',
-            'attribute_id': self.attribute2.id,
-            'value_ids': False,
-            })]
+        wizard.line_ids = [
+            (
+                0,
+                0,
+                {
+                    "action": "replace",
+                    "attribute_id": self.attribute2.id,
+                    "value_ids": False,
+                },
+            )
+        ]
         wizard.confirm()
         for product in self.product_tmpl_ids:
             attribute_line_id = self.product1.attribute_line_ids.filtered(
-                lambda l: l.attribute_id == self.attribute2)
+                lambda l: l.attribute_id == self.attribute2
+            )
             self.assertFalse(attribute_line_id)
 
     def test_variant_create(self):
         # Test adding new values should fail without force
         wizard = self.get_wizard()
-        wizard.line_ids = [(0, 0, {
-            'action': 'add',
-            'attribute_id': self.attribute1.id,
-            'value_ids': self.attribute1.value_ids,
-            })]
+        wizard.line_ids = [
+            (
+                0,
+                0,
+                {
+                    "action": "add",
+                    "attribute_id": self.attribute1.id,
+                    "value_ids": self.attribute1.value_ids,
+                },
+            )
+        ]
         with self.assertRaises(UserError):
             wizard.confirm()
 
         # Test adding new values should succeed with force
         wizard = self.get_wizard()
-        wizard.line_ids = [(0, 0, {
-            'action': 'add',
-            'attribute_id': self.attribute1.id,
-            'value_ids': self.attribute1.value_ids,
-            })]
+        wizard.line_ids = [
+            (
+                0,
+                0,
+                {
+                    "action": "add",
+                    "attribute_id": self.attribute1.id,
+                    "value_ids": self.attribute1.value_ids,
+                },
+            )
+        ]
         wizard.force = True
         wizard.confirm()
 
         for product in self.product_tmpl_ids:
             attribute_line_id = self.product1.attribute_line_ids.filtered(
-                lambda l: l.attribute_id == self.attribute1)
+                lambda l: l.attribute_id == self.attribute1
+            )
             self.assertEqual(
-                attribute_line_id.value_ids, self.attribute1.value_ids)
+                attribute_line_id.value_ids, self.attribute1.value_ids
+            )
--- product_attribute_mass_edit/wizards/product_attribute_mass_edit.py
+++ product_attribute_mass_edit/wizards/product_attribute_mass_edit.py
@@ -6,152 +6,188 @@ from odoo.exceptions import UserError
 
 
 class ProductAttributeMassEditWizard(models.TransientModel):
-    _name = 'product.attribute.mass.edit.wizard'
-    _description = 'Product Attribute Mass Edit Wizard'
+    _name = "product.attribute.mass.edit.wizard"
+    _description = "Product Attribute Mass Edit Wizard"
 
     product_tmpl_ids = fields.Many2many(
-        'product.template',
-        string='Product Templates',
-        readonly=True,
+        "product.template", string="Product Templates", readonly=True,
     )
 
     line_ids = fields.One2many(
-        'product.attribute.mass.edit.wizard.line',
-        'wizard_id',
-        'Operations',
+        "product.attribute.mass.edit.wizard.line",
+        "wizard_id",
+        "Operations",
         required=True,
     )
 
     force = fields.Boolean(
-        'Allow to modify attributes that create/delete variants')
+        "Allow to modify attributes that create/delete variants"
+    )
 
     @api.model
     def default_get(self, fields):
         res = super().default_get(fields)
-        res_ids = self._context.get('active_ids')
-        res.update({
-            'product_tmpl_ids': res_ids,
-        })
+        res_ids = self._context.get("active_ids")
+        res.update(
+            {"product_tmpl_ids": res_ids,}
+        )
         return res
 
     def validate_safe(self):
         self.ensure_one()
         for line in self.line_ids:
-            if line.attribute_id.create_variant == 'always':
-                raise UserError(_(
-                    'Unable to mass edit attributes that create variants\n'
-                    'You\'re trying to edit attribute "%s", which always '
-                    'creates new variants.'
-                    'This is not recommended. If you really need to do it '
-                    'run the wizard in debug mode and enable the option.'
-                    ) % line.attribute_id.name)
-            elif line.attribute_id.create_variant == 'dynamic':
-                if line.action != 'add':
-                    raise UserError(_(
-                        'Unable to mass edit attributes that create variants\n'
-                        'You\'re trying to remove or replace values for "%s", '
-                        'which creates variants dynamically.\n'
-                        'This is not recommended. If you really need to do it '
-                        'run the wizard in debug mode and enable the option.'
-                        ) % line.attribute_id.name)
+            if line.attribute_id.create_variant == "always":
+                raise UserError(
+                    _(
+                        "Unable to mass edit attributes that create variants\n"
+                        'You\'re trying to edit attribute "%s", which always '
+                        "creates new variants."
+                        "This is not recommended. If you really need to do it "
+                        "run the wizard in debug mode and enable the option."
+                    )
+                    % line.attribute_id.name
+                )
+            elif line.attribute_id.create_variant == "dynamic":
+                if line.action != "add":
+                    raise UserError(
+                        _(
+                            "Unable to mass edit attributes that create variants\n"
+                            'You\'re trying to remove or replace values for "%s", '
+                            "which creates variants dynamically.\n"
+                            "This is not recommended. If you really need to do it "
+                            "run the wizard in debug mode and enable the option."
+                        )
+                        % line.attribute_id.name
+                    )
 
     @api.multi
     def confirm(self):
         self.ensure_one()
         # prefetch
-        self.product_tmpl_ids.mapped('attribute_line_ids.value_ids.id')
+        self.product_tmpl_ids.mapped("attribute_line_ids.value_ids.id")
         self.product_tmpl_ids.mapped(
-            'attribute_line_ids.attribute_id.create_variant')
+            "attribute_line_ids.attribute_id.create_variant"
+        )
         if not self.force:
             self.validate_safe()
         for product_tmpl_id in self.product_tmpl_ids:
             self.update_attribute_lines(product_tmpl_id)
 
     def update_attribute_lines(self, product_tmpl_id):
-        ''' We need to compute everything and apply a single write.
-        This way create_variant_ids is called only once per template '''
+        """ We need to compute everything and apply a single write.
+        This way create_variant_ids is called only once per template """
         operations = []
         for line in self.line_ids:
             current_line = product_tmpl_id.attribute_line_ids.filtered(
-                lambda l: l.attribute_id == line.attribute_id)
-            if line.action == 'add':
+                lambda l: l.attribute_id == line.attribute_id
+            )
+            if line.action == "add":
                 if not current_line:
                     # Create new attribute line
-                    operations += [(0, 0, {
-                        'attribute_id': line.attribute_id.id,
-                        'value_ids': [(6, 0, line.value_ids.ids)],
-                    })]
+                    operations += [
+                        (
+                            0,
+                            0,
+                            {
+                                "attribute_id": line.attribute_id.id,
+                                "value_ids": [(6, 0, line.value_ids.ids)],
+                            },
+                        )
+                    ]
                 else:
                     # Add values to the existing line
-                    operations += [(1, current_line.id, {
-                        'value_ids': [
-                            (4, v.id) for v in line.value_ids
-                            if v not in current_line.value_ids
-                        ]
-                    })]
-            if line.action == 'remove':
+                    operations += [
+                        (
+                            1,
+                            current_line.id,
+                            {
+                                "value_ids": [
+                                    (4, v.id)
+                                    for v in line.value_ids
+                                    if v not in current_line.value_ids
+                                ]
+                            },
+                        )
+                    ]
+            if line.action == "remove":
                 if current_line:
                     if len(current_line.value_ids - line.value_ids) == 0:
                         # Remove attribute line
                         operations += [(2, current_line.id)]
                     else:
                         # Remove values from line
-                        operations += [(1, current_line.id, {
-                            'value_ids': [
-                                (3, v.id) for v in line.value_ids
-                                if v in current_line.value_ids
-                            ]
-                        })]
-            if line.action == 'replace':
+                        operations += [
+                            (
+                                1,
+                                current_line.id,
+                                {
+                                    "value_ids": [
+                                        (3, v.id)
+                                        for v in line.value_ids
+                                        if v in current_line.value_ids
+                                    ]
+                                },
+                            )
+                        ]
+            if line.action == "replace":
                 if current_line and line.value_ids:
                     # Replace values from existing line
-                    operations += [(1, current_line.id, {
-                        'value_ids': [(6, 0, line.value_ids.ids)],
-                    })]
+                    operations += [
+                        (
+                            1,
+                            current_line.id,
+                            {"value_ids": [(6, 0, line.value_ids.ids)],},
+                        )
+                    ]
                 elif current_line and not line.value_ids:
                     # Remove attribute line
                     operations += [(2, current_line.id)]
                 elif not current_line and line.value_ids:
                     # Create new attribute line
-                    operations += [(0, 0, {
-                        'attribute_id': line.attribute_id.id,
-                        'value_ids': [(6, 0, line.value_ids.ids)],
-                    })]
+                    operations += [
+                        (
+                            0,
+                            0,
+                            {
+                                "attribute_id": line.attribute_id.id,
+                                "value_ids": [(6, 0, line.value_ids.ids)],
+                            },
+                        )
+                    ]
         if operations:
-            product_tmpl_id.write({'attribute_line_ids': operations})
+            product_tmpl_id.write({"attribute_line_ids": operations})
 
 
 class ProductAttributeMassEditWizardLine(models.TransientModel):
-    _name = 'product.attribute.mass.edit.wizard.line'
-    _description = 'Product Attribute Mass Edit Wizard Line'
+    _name = "product.attribute.mass.edit.wizard.line"
+    _description = "Product Attribute Mass Edit Wizard Line"
 
     wizard_id = fields.Many2one(
-        'product.attribute.mass.edit.wizard',
+        "product.attribute.mass.edit.wizard",
         readonly=True,
         required=True,
         index=True,
-        ondelete='cascade',
+        ondelete="cascade",
     )
 
-    force = fields.Boolean(related='wizard_id.force')
+    force = fields.Boolean(related="wizard_id.force")
 
-    action = fields.Selection([
-        ('add', 'Add values'),
-        ('remove', 'Remove values'),
-        ('replace', 'Replace values'),
+    action = fields.Selection(
+        [
+            ("add", "Add values"),
+            ("remove", "Remove values"),
+            ("replace", "Replace values"),
         ],
-        string='Action',
+        string="Action",
         required=True,
     )
 
     attribute_id = fields.Many2one(
-        'product.attribute',
-        'Attribute',
-        required=True,
+        "product.attribute", "Attribute", required=True,
     )
 
     value_ids = fields.Many2many(
-        'product.attribute.value',
-        'product_attribute_mass_edit_wizard_line_pav_rel',
-        string='Values',
+        "product.attribute.value",
+        "product_attribute_mass_edit_wizard_line_pav_rel",
+        string="Values",
     )
