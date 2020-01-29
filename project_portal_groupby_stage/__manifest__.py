affdee2a55a38bfea7356e8bb9dfc771612724ca
Krunal Prajapati <krunal267@gmail.com>
2020-01-29 17:02:17 +0530

    [ADD] Added new module project_portal_groupby_stage (#32)
    * [ADD] Added new module project_portal_groupby_stage

---
+++ project_portal_groupby_stage/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Group by Stage in Tasks (Portal)',
+    'summary': 'Group by Stage in Tasks view of Portal',
+    'category': 'Project',
+    'version': '12.0.1.0.0',
+    'author': 'Druidoo',
+    'license': 'AGPL-3',
+    'depends': [
+        'project',
+    ],
+    'data': [
+        'views/task_portal_templates.xml',
+    ],
+}
