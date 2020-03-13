834fbb268ebeacdc7bf3120d811ee064d220a652
Chetan R Dhaduk <scrdhaduk@gmail.com>
2020-03-13 15:54:24 +0530

    [ADD]Added account_report_invoice_paid module. (#38)
    * [ADD] Added account_report_invoice_paid module.
    
    Co-authored-by: Iván Todorovich <ivan.todorovich@druidoo.io>

---
+++ account_report_invoice_paid/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2020-Today: Druidoo (<http://www.druidoo.io/>)
+# @author: Druidoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html
+
+{
+    'name': 'Account Invoice Report Paid',
+    'summary': '',
+    'version': '12.0.1.0.0',
+    'category': 'Accounting',
+    'author': 'Druidoo',
+    'website': 'http://www.druidoo.io',
+    'license': 'AGPL-3',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/report_invoice_templates.xml',
+    ],
+    'application': False,
+    'installable': True,
+}
