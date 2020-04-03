74556e28c91042654bb92bbed686783266557d28
Krunal <krunal267@gmail.com>
2020-04-03 10:37:55 +0530

    [ADD] Added new module pos_account_invoice_report_stock_lot

---
+++ pos_account_invoice_report_stock_lot/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Stock Lot numbers from POS Orders in Invoice report',
+    'summary': '',
+    'version': '12.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'author': "Druidoo",
+    'website': 'http://www.druidoo.io',
+    'license': "AGPL-3",
+    'depends': [
+        'account_invoice_report_stock_lot',
+        'point_of_sale',
+    ],
+    'data': [
+    ],
+    'auto_install': True,
+}
