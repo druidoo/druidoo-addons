3774243a1ba7a51c147c177ed1949ee9b8401195
Chetan <scrdhaduk@gmail.com>
2020-05-13 11:28:27 +0530

    [MIG] Migrate project_task_notify_state_changed to v13

--- project_task_notify_state_changed/__manifest__.py
+++ project_task_notify_state_changed/__manifest__.py
@@ -1,14 +1,10 @@
 {
-    'name': 'notify task stage changed',
-    'version': '12.0.1.0.0',
-    'category': 'Druidoo',
-    'website': 'https://github.com/druidoo/druidoo-addons',
-    'author': 'Druidoo',
-    'license': 'AGPL-3',
-    'depends': [
-        'project',
-    ],
-    'data': [
-        'views/project_views.xml',
-    ],
+    "name": "notify task stage changed",
+    "version": "13.0.1.0.0",
+    "category": "Druidoo",
+    "website": "https://github.com/druidoo/druidoo-addons",
+    "author": "Druidoo",
+    "license": "AGPL-3",
+    "depends": ["project"],
+    "data": ["views/project_views.xml"],
 }
