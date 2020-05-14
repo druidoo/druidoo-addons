26dd5a4fd2d37950d62a4c4d0b471b03c487a663
Chetan <scrdhaduk@gmail.com>
2020-05-14 15:34:25 +0530

    [MIG] Migrate project_portal_groupby_stage module to v13

--- project_portal_groupby_stage/__manifest__.py
+++ project_portal_groupby_stage/__manifest__.py
@@ -1,14 +1,13 @@
 {
-    'name': 'Group by Stage in Tasks (Portal)',
-    'summary': 'Group by Stage in Tasks view of Portal',
-    'category': 'Project',
-    'version': '12.0.1.0.0',
-    'author': 'Druidoo',
-    'license': 'AGPL-3',
-    'depends': [
-        'project',
+    "name": "Group by Stage in Tasks (Portal)",
+    "summary": "Group by Stage in Tasks view of Portal",
+    "category": "Project",
+    "version": "13.0.1.0.0",
+    "author": "Druidoo",
+    "license": "AGPL-3",
+    "depends": [
+        "project",
+        "website"
     ],
-    'data': [
-        'views/task_portal_templates.xml',
-    ],
+    "data": ["views/task_portal_templates.xml"],
 }
