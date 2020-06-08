bfdd2cf0458c3f89b744fb72ac003444080a811e
Iván Todorovich <ivan.todorovich@druidoo.io>
2020-06-08 17:44:56 +0200

    [FIX] Error in default value

--- pos_order_mgmt_draft_downpayment/models/pos_config.py
+++ pos_order_mgmt_draft_downpayment/models/pos_config.py
@@ -1,19 +1,30 @@
 # Copyright 2019 Druidoo - Iván Todorovich
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import api, models, fields
 
 
 class PosConfig(models.Model):
     _inherit = 'pos.config'
 
     iface_downpayment = fields.Boolean('Enable Downpayments')
-
+    
     deposit_product_id = fields.Many2one(
         'product.product',
         string='Deposit Product',
         default=lambda self:
             self.env['product.product'].browse(
-                self.env['ir.config_parameter'].sudo().get_param(
-                    'sale.default_deposit_product_id')).exists()
+                int(self.env['ir.config_parameter'].sudo().get_param(
+                    'sale.default_deposit_product_id'))).exists()
     )
+
+    @api.model
+    def _default_deposit_product_id(self):
+        get_param = self.env['ir.config_parameter'].sudo().get_param
+        deposit_product_id = get_param('sale.default_deposit_product_id')
+        try:
+            deposit_product_id = int(deposit_product_id)
+            product = self.env['product.product'].browse(deposit_product_id)
+            return product.exists()
+        except Exception as e:
+            return False
